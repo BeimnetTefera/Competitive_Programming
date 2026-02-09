e128e18ca0b5f061620a05156b35c744f389f690
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-09 08:38:13 +0300

    Update B_Balance_the_Colonies.py

--- CodeForce/Contests/Solutions/B_Balance_the_Colonies.py
+++ CodeForce/Contests/Solutions/B_Balance_the_Colonies.py
@@ -1,11 +1,9 @@
-from math import ceil, floor
 t = int(input())
 for _ in range(t):
     num = int(input())
-    if num <= 2:
-        print(2)
+    if num <= 3:
+        print(num)
+    elif num % 2 == 0:
+        print(0)
     else:
-        rem = num % 3
-        val = num // 3
-        result = (floor(val) * 3) - (ceil(val) * 3) + rem
-        print(result)
+        print(1)
