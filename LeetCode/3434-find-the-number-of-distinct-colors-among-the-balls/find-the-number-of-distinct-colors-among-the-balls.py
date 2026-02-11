4e4e0e28edfc1dbe07b283dd613b970bb863d544
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-11 13:10:45 +0300

    Time: 67 ms (89.00%) | Memory: 66.3 MB (44.33%) - LeetSync

--- LeetCode/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
+++ LeetCode/3434-find-the-number-of-distinct-colors-among-the-balls/find-the-number-of-distinct-colors-among-the-balls.py
@@ -1,62 +1,5 @@
 class Solution:
     def queryResults(self, limit: int, queries: List[List[int]]) -> List[int]:
-        color_freq = {}
-        ball_color = {}
-        result = []
-
-        for ball, color in queries:
-            if ball in ball_color:
-                prev = ball_color[ball]
-
-                color_freq[prev] -= 1
-                if color_freq[prev] == 0:
-                    del color_freq[prev]
-            
-                ball_color[ball] = color
-
-                if color in color_freq:
-                    color_freq[color] += 1
-                else:
-                    color_freq[color] = 1
-            else:
-                ball_color[ball] = color
-                if color in color_freq:
-                    color_freq[color] += 1
-                else:
-                    color_freq[color] = 1
-
-            result.append(len(color_freq))
-
-        return result
-
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         """
         create two hashmap to track
             - ball_color
@@ -69,43 +12,44 @@ class Solution:
                 - update the color freq in color_freq
                         if freq == 0:
                             remove form the dictionary
-                            
+
                 - update the ball color ib ball_color
                 - update color freq in color_freq
                         - if color exist increment by one
                         - else add new color with freq = 1
                 N0:
-                - update the ball in ball_color 
+                - add the ball in ball_color 
                 - update color freq in color_freq
                         - if color exist increment by one
                         - else add new color with freq = 1
         
         """
+        color_freq = {}
+        ball_color = {}
+        result = []
 
+        for ball, color in queries:
+            # check if ball in ball_color dictionary
+            if ball in ball_color:
+                # for readability store the ball color in prev variable
+                prev = ball_color[ball]
 
-        # ball_to_color = {}
-        # color_to_freq = {}
-        # result = []
+                # decrement the color freq from ball_freq becuase if it that ball has different color previously know we have to decrement the previous count
+                color_freq[prev] -= 1
+                # if the color freq is 0, no ball is assigned to it so we should remove it 
+                if color_freq[prev] == 0:
+                    del color_freq[prev]
+            
+            # then if all the sanity check on the upper block is passed we update the color of the ball and its color
+            ball_color[ball] = color
 
-        # for qry in queries:
-        #     ball, color = qry
+            # update the freq of the new color 
+            if color in color_freq:
+                color_freq[color] += 1
+            else:
+                color_freq[color] = 1
 
-        #     if ball in ball_to_color and ball_to_color[ball] != color:
-        #         color_to_freq[ball_to_color[ball]] -= 1
+            # we store the length of the color_freq in result 
+            result.append(len(color_freq))
 
-        #         if color_to_freq[ball_to_color[ball]] == 0:
-        #             del color_to_freq[ball_to_color[ball]]
-                    
-        #     elif ball in ball_to_color and ball_to_color[ball] == color:
-        #         result.append(len(color_to_freq))
-        #         continue
-
-        #     if color in color_to_freq:
-        #         color_to_freq[color] += 1
-        #     else:
-        #         color_to_freq[color] = 1
-
-        #     ball_to_color[ball] = color
-        #     result.append(len(color_to_freq))
-
-        # return result
+        return result
