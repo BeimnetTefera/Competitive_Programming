706a82e68e328fef7d006e46d5b6158cfceabe72
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-23 17:13:15 +0300

    Time: 4 ms (21.27%) | Memory: 19.4 MB (31.97%) - LeetSync

--- LeetCode/919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.py
+++ LeetCode/919-projection-area-of-3d-shapes/projection-area-of-3d-shapes.py
@@ -1,5 +1,6 @@
 class Solution:
     def projectionArea(self, grid: List[List[int]]) -> int:
+
         rows = len(grid)
         cols = len(grid[0])
 
@@ -8,7 +9,7 @@ class Solution:
             for col in range(cols):
                 if grid[row][col] != 0:
                     top += 1
-                    
+
 
         yz = []
         for row in range(rows):
@@ -16,15 +17,9 @@ class Solution:
             for col in range(cols):
                 if grid[row][col] > max_row:
                     max_row = grid[row][col]
-
             yz.append(max_row)
 
-        min_yz = min(yz)
-        left = 0
-        for val in yz:
-            left += (val - min_yz)
-
-        left += (min_yz * rows)
+        left = sum(yz)
 
         xy = []
         for col in range(cols):
@@ -32,14 +27,8 @@ class Solution:
             for row in range(rows):
                 if grid[row][col] > max_col:
                     max_col = grid[row][col]
-
             xy.append(max_col)
 
-        min_xy = min(xy)
-        right = 0
-        for val in xy:
-            right += (val - min_xy)
-
-        right += (min_xy * cols)
+        right = sum(xy)
 
         return left + right + top
