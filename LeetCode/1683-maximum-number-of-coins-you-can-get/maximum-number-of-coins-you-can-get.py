acac357399cb8fa894e7589459b6681c76c7a7ee
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-02-27 21:47:49 +0300

    Time: 68 ms (72.56%) | Memory: 29.6 MB (69.55%) - LeetSync

--- LeetCode/1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
+++ LeetCode/1683-maximum-number-of-coins-you-can-get/maximum-number-of-coins-you-can-get.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxCoins(self, piles: List[int]) -> int:
-        piles.sort()
+        size = len(piles)
         score = 0
 
-        size = len(piles)
-
-        step = size / 3
+        piles.sort()
+        
+        step = size // 3
         turn = size - 2
 
         while step > 0:
@@ -13,5 +13,5 @@ class Solution:
             score += piles[turn]
             step -= 1
             turn -= 2
-            
+
         return score
