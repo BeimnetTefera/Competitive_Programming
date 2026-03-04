e2f42760406439cd8293dbfbb3042cbec2101637
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-03-04 17:42:31 +0300

    Add solution for Third Maximum Number (trial 1)

---
+++ leetcode/third-maximum-number_trial1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        arr = sorted(set(nums), reverse = True)
+
+        if len(arr) < 3:
+            return arr[0]
+        else:
+            return arr[2]
