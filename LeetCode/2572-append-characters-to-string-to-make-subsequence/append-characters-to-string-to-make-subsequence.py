d6f110fa087a3838344a6921a4cc3680fd878a90
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-03-08 22:07:08 +0300

    Time: 31 ms (39.60%) | Memory: 19.9 MB (57.94%) - LeetSync

--- LeetCode/2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
+++ LeetCode/2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.py
@@ -2,8 +2,7 @@ class Solution:
     def appendCharacters(self, s: str, t: str) -> int:
         p1 = 0
         p2 = 0
-
-        cnt = 0
+        
         while p1 < len(s) and p2 < len(t):
             if s[p1] == t[p2]:
                 p1 += 1
