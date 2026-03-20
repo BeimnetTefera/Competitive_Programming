6497bf3052d32bf7502223047caeeeb34c4addb7
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-03-20 22:11:04 +0300

    Time: 7 ms (56.39%) | Memory: 26.4 MB (84.66%) - LeetSync

--- LeetCode/189-rotate-array/rotate-array.py
+++ LeetCode/189-rotate-array/rotate-array.py
@@ -3,9 +3,22 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        while k > 0:
-            val = nums.pop()
-            nums.insert(0, val)
-            k -= 1
-
+        nums.reverse()
+        k = k % len(nums)
+        mov = k // 2
+        last = k - 1
+        for i in range(k):
+            if mov > 0:
+                nums[i], nums[last] = nums[last], nums[i]
+                last -= 1
+                mov -= 1
+            
+        cnt = (len(nums) - k) // 2
+        right = len(nums) - 1
+        for j in range(k, len(nums)):
+            if cnt > 0:
+                nums[j], nums[right] = nums[right], nums[j]
+                cnt -= 1
+                right -= 1
+        
         return nums
