97a3c01ff39f831b908d429a2bad6bbb20fd95ee
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-03-22 19:48:40 +0300

    Time: 142 ms (34.60%) | Memory: 37.6 MB (28.44%) - LeetSync

---
+++ LeetCode/2119-minimum-number-of-operations-to-make-array-continuous/minimum-number-of-operations-to-make-array-continuous.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        length = len(nums)
+        nums = sorted(set(nums))
+        res = length
+        right = 0
+
+        for left in range(len(nums)):
+            while right < len(nums) and nums[right] < nums[left] + length:
+                right += 1
+
+            window = right - left
+            res = min(res, length - window)
+
+        return res
