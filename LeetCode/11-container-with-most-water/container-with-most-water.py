036503981ec78bab19773f2cf9087fe1e1a52929
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-03-23 10:39:55 +0300

    Time: 53 ms (78.61%) | Memory: 29.8 MB (28.49%) - LeetSync

--- LeetCode/11-container-with-most-water/container-with-most-water.py
+++ LeetCode/11-container-with-most-water/container-with-most-water.py
@@ -1,25 +1,21 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        max_capacity = 0
-
         left = 0
         right = len(height) - 1
+        max_capacity = 0
 
-        while left <  right:
-            # dimentions
+        while left < right:
+
             width = right - left
-            length = min(height[left], height[right])
 
-            # calculation
-            area = length * width
-
-            if area > max_capacity:
-                max_capacity = area
-
-            # always smaller one moves
-            if height[left] < height[right]:
+            if height[left] <= height[right]:
+                cur_capacity = height[left] * width
                 left += 1
+
             else:
+                cur_capacity = height[right] * width
                 right -= 1
+
+            max_capacity = max(max_capacity, cur_capacity)
 
         return max_capacity
