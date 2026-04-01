5f05be326d737387d1e784d55d70421cb24debd3
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-01 12:18:44 +0300

    Time: 50 ms (77.77%) | Memory: 19.4 MB (98.05%) - LeetSync

---
+++ LeetCode/680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        #  left and right start at the back
+        # the pointers move until they cross eachother
+        # count when characters don't match until it exceds one
+
+        left, right = 0, len(s) - 1
+        while left < right:
+
+            if s[left] != s[right]:
+                l1, r1 = left + 1, right
+                l2, r2 = left , right - 1
+
+                while l1 < r1 and s[l1] == s[r1]:
+                    l1 += 1
+                    r1 -= 1
+
+                while l2 < r2 and s[l2] == s[r2]:
+                    l2 += 1
+                    r2 -= 1
+
+                return l1 >= r1 or l2 >= r2
+
+            left += 1
+            right -= 1
+
+        return True
