fdcbe743670ead9646eecd07f5f13fa0ffe8e02a
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-15 15:16:40 +0300

    Time: 19 ms (5.19%) | Memory: 39.9 MB (5.30%) - LeetSync

--- LeetCode/344-reverse-string/reverse-string.py
+++ LeetCode/344-reverse-string/reverse-string.py
@@ -1,15 +1,10 @@
 class Solution(object):
     def reverseString(self, s):
-        """
-        :type s: List[str]
-        :rtype: None Do not return anything, modify s in-place instead.
-        """
-        left = 0
-        right = len(s) - 1
+        return self.helper(s, 0, len(s) - 1)
 
-        while right > left:
-            s[right], s[left] = s[left], s[right]
-            right -= 1
-            left += 1
+    def helper(self, s, left, right):
+        if left >= right:
+            return 
 
-        return s
+        s[left], s[right] = s[right], s[left]
+        return self.helper(s, left + 1, right - 1)
