fe6ee7d2eb50a3df1a5d576ec5618da841b7149c
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-16 23:05:17 +0300

    Time: 3 ms (21.98%) | Memory: 19.3 MB (33.94%) - LeetSync

---
+++ LeetCode/2050-count-good-numbers/count-good-numbers.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countGoodNumbers(self, n: int) -> int:
+        MOD = 10**9 + 7
+        prime_count = n // 2
+        even_count = n - prime_count
+
+        total = (pow(5, even_count, MOD) * pow(4, prime_count, MOD)) % MOD
+        return total
