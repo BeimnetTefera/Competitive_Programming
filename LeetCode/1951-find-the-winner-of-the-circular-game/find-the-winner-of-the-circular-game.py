9c16abad308ed55f469c26b34a401f00141deb11
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-19 18:15:34 +0300

    Time: 451 ms (5.05%) | Memory: 60.9 MB (10.73%) - LeetSync

--- LeetCode/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
+++ LeetCode/1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.py
@@ -1,18 +1,20 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        deq = deque()
-        for i in range(1, n + 1):
-            deq.append(i)
-
+        deq = deque(range(1, n+1))
         count = 0
 
-        while len(deq) > 1:
+        def helper(deq, count):
+
+            if len(deq) == 1:
+                return deq[0]
+
             val = deq.popleft()
             count += 1
 
             if k == count:
-                count = 0
-                continue
-
-            deq.append(val)
-        return deq[0]
+                return helper(deq, 0)
+            else:
+                deq.append(val)
+                return helper(deq, count)
+        
+        return helper(deq, count)
