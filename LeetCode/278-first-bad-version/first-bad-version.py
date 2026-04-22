3587c073aa23156fa1a55609607dba3e6ab18661
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-22 09:41:01 +0300

    Time: 31 ms (99.19%) | Memory: 19.1 MB (92.83%) - LeetSync

--- LeetCode/278-first-bad-version/first-bad-version.py
+++ LeetCode/278-first-bad-version/first-bad-version.py
@@ -4,18 +4,17 @@
 class Solution:
     def firstBadVersion(self, n: int) -> int:
 
-        left = 0
+        left = 1
         right = n
         ans = float("inf")
-        while left <= right:
-            mid = left + (right - left) // 2
-            if isBadVersion(mid):
-                if mid < ans:
-                    ans = mid
 
-                right = mid - 1
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isBadVersion(mid):
+                right = mid 
             else:
                 left = mid + 1
 
-        return ans
+        return right
                 
