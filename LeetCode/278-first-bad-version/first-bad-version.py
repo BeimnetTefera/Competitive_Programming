79e0d153061f40dd8c67b8d1a68603ff93b1fcc6
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-22 09:29:45 +0300

    Time: 52 ms (9.67%) | Memory: 19.2 MB (68.80%) - LeetSync

--- LeetCode/278-first-bad-version/first-bad-version.py
+++ LeetCode/278-first-bad-version/first-bad-version.py
@@ -6,12 +6,16 @@ class Solution:
 
         left = 0
         right = n
-        while left < right:
-            mid = (left + right) // 2
-
+        ans = float("inf")
+        while left <= right:
+            mid = left + (right - left) // 2
             if isBadVersion(mid):
-                right = mid 
+                if mid < ans:
+                    ans = mid
+
+                right = mid - 1
             else:
                 left = mid + 1
 
-        return left
+        return ans
+                
