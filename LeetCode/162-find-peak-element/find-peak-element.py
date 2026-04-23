eabc518dcbabf89c2de893f2b1f602d9a1897897
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-23 09:51:30 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (81.64%) - LeetSync

--- LeetCode/162-find-peak-element/find-peak-element.py
+++ LeetCode/162-find-peak-element/find-peak-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
         nums.append(float("-inf"))
+        # if first element greater than elment next to it print its index
         if nums[0] > nums[1]:
             return 0
-        idx = 0
+
         cur, right = 1, 2
         while right < len(nums):
+
+            # check if it is greater than its neighbours
             if nums[cur] > nums[cur - 1] and nums[cur] > nums[right]:
-                idx = cur
-                return idx
+                return cur
+            # move pointer
             cur += 1
             right += 1
