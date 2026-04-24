d5a9d509dc5fee202651d3d256d31b6559ca8f1f
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-04-24 22:58:34 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (35.25%) - LeetSync

---
+++ LeetCode/3447-clear-digits/clear-digits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack =  []
+        for char in s:
+            if char.isalpha():
+                stack.append(char)
+            elif stack:
+                stack.pop()
+
+        return "".join(stack)
