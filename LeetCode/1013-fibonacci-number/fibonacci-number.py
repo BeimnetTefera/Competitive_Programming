307f0d8d59e237034adcfbd2d68a2f9630fd87ee
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-01 14:47:17 +0300

    Time: 282 ms (33.56%) | Memory: 19 MB (89.15%) - LeetSync

--- LeetCode/1013-fibonacci-number/fibonacci-number.py
+++ LeetCode/1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,19 @@
 class Solution:
     def fib(self, n: int) -> int:
+
         if n <= 1:
             return n
 
-        res = self.fib(n-1) + self.fib(n-2)
+        return self.fib(n-2) + self.fib(n-1)
 
-        return res
+
+
+"""
+n = 5
+fib(5) = fib(3) + fib(4)
+fib(4) = 1 + fib(3)
+fib(2) = fib(1) + fib(0)
+
+fib(3) = [2]
+
+"""
