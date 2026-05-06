2ae7bfb1e626e6ef687f3a20a4f762df2b4a155a
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-06 15:39:31 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (54.97%) - LeetSync

--- LeetCode/50-powx-n/powx-n.py
+++ LeetCode/50-powx-n/powx-n.py
@@ -1,3 +1,25 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return pow(x, n)
+        sign = 1
+        
+        if n < 0:
+            sign = -1
+            n = n * -1
+    
+        def helper(x, n):
+            if n == 0:
+                return 1
+
+            half = helper(x, n //2)
+            
+            if n % 2 == 0:
+                return half * half
+            else:
+                return x * half * half
+
+        value = helper(x,n)
+
+        if sign < 0:
+            return 1 / value
+        else:
+            return value
