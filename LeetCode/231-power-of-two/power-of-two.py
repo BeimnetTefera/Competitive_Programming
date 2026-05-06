0dab21cf3e949fb9ebc246ca7cd65df9c7b8e344
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-06 15:58:07 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (6.45%) - LeetSync

--- LeetCode/231-power-of-two/power-of-two.py
+++ LeetCode/231-power-of-two/power-of-two.py
@@ -1,17 +1,16 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-
         if n <= 0:
             return False
 
-        def helper (n):
-
+        def helper( n):
+            # base case
             if n == 1:
                 return True
-
+            #  if it is odd return no need to check
             if n % 2 != 0:
                 return False
 
-            return helper(n//2)
+            return helper(n // 2)
 
         return helper(n)
