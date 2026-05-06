49c60301dc2c089af89132e5c47ef409fff7e02b
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-06 14:40:20 +0300

    Time: 304 ms (27.36%) | Memory: 19.1 MB (89.16%) - LeetSync

--- LeetCode/1013-fibonacci-number/fibonacci-number.py
+++ LeetCode/1013-fibonacci-number/fibonacci-number.py
@@ -1,19 +1,10 @@
 class Solution:
     def fib(self, n: int) -> int:
-
+        # base case
         if n <= 1:
             return n
 
-        return self.fib(n-2) + self.fib(n-1)
+        first = self.fib(n-1)
+        second = self.fib(n-2)
 
-
-
-"""
-n = 5
-fib(5) = fib(3) + fib(4)
-fib(4) = 1 + fib(3)
-fib(2) = fib(1) + fib(0)
-
-fib(3) = [2]
-
-"""
+        return first + second
