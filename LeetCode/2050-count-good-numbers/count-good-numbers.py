525b2147af65d0cf4f0571d24b06c3d7afc0e7e5
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-08 19:55:09 +0300

    Time: 3 ms (23.18%) | Memory: 19.3 MB (25.79%) - LeetSync

--- LeetCode/2050-count-good-numbers/count-good-numbers.py
+++ LeetCode/2050-count-good-numbers/count-good-numbers.py
@@ -2,8 +2,30 @@ class Solution:
     def countGoodNumbers(self, n: int) -> int:
         MOD = 10**9 + 7
         
-        prime_count = n // 2
-        even_count = n - prime_count
+        def helper(power, num):
+            # base case
+            if power == 0:
+                return 1
 
-        total = (pow(5, even_count, MOD) * pow(4, prime_count, MOD)) % MOD
-        return total
+            half_res = helper(power // 2, num)
+
+            if power % 2 == 0:
+                return (half_res * half_res) % MOD
+            else:
+                return (half_res * half_res * num) % MOD
+
+        # check if it is even or odd
+        is_even = False
+        if n % 2 == 0:
+            is_even = True
+        
+        power = n // 2
+
+        # do the recursion for the half of them
+        res1 = helper(power, 5)
+        res2 = helper(power, 4)
+
+        if is_even:
+            return (res1 * res2) % MOD
+        else:
+            return (res1 * res2 * 5) % MOD
