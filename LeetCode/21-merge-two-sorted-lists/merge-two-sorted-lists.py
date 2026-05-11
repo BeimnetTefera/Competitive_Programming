e6fd61656c5c90a2d34e5645268031cddae4af06
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-11 16:17:00 +0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (6.36%) - LeetSync

--- LeetCode/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ LeetCode/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,41 +5,31 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        infinity = float("inf")
-        temp1 = list1
-        temp2 = list2
+        # cur and head are pointers to newly created linked list
+        dummy = ListNode(-1)
+        cur = dummy
+        # assign the pointers
+        ptr1 = list1
+        ptr2 = list2
+        # compare the two values and form the array
+        while ptr1 and ptr2:
 
-        if not list1:
-            list1 = ListNode(infinity)
-        if not list2:
-            list2 = ListNode(infinity)
+            if ptr1.val >= ptr2.val:
+                cur.next = ptr2
+                ptr2 = ptr2.next
 
-        while temp1 and temp1.next:
-            temp1 = temp1.next
+            else:
+                cur.next = ptr1
+                ptr1 = ptr1.next
 
-        if temp1:
-            temp1.next = ListNode(infinity)
+            # move the pointer
+            cur = cur.next
 
-        while temp2 and temp2.next:
-            temp2 = temp2.next
+        # if the traverse didn't end
+        if ptr1:
+            cur.next = ptr1
 
-        if temp2:
-            temp2.next = ListNode(infinity)
+        elif ptr2:
+            cur.next = ptr2
 
-        temp3 = list1
-        temp4 = list2
-        result = ListNode(0)
-        cur = result
-        while temp3 and temp4 and cur:
-            if temp3.val <= temp4.val and temp3.val != infinity:
-                cur.next = ListNode(temp3.val)
-                temp3 = temp3.next
-
-            elif temp4.val < temp3.val and temp4.val != infinity:
-                cur.next = ListNode(temp4.val)
-                temp4 = temp4.next
-
-            if cur:
-                cur = cur.next
-
-        return result.next
+        return dummy.next
