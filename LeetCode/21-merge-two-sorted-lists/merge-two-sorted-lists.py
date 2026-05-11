03a3d661ea8ca9cb7983fea305130bcf007d8223
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-11 17:00:17 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.68%) - LeetSync

--- LeetCode/21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ LeetCode/21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,31 +5,17 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        # cur and head are pointers to newly created linked list
-        dummy = ListNode(-1)
-        cur = dummy
-        # assign the pointers
-        ptr1 = list1
-        ptr2 = list2
-        # compare the two values and form the array
-        while ptr1 and ptr2:
+        if not list1:
+            return list2
 
-            if ptr1.val >= ptr2.val:
-                cur.next = ptr2
-                ptr2 = ptr2.next
+        if not list2:
+            return list1
 
-            else:
-                cur.next = ptr1
-                ptr1 = ptr1.next
 
-            # move the pointer
-            cur = cur.next
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
 
-        # if the traverse didn't end
-        if ptr1:
-            cur.next = ptr1
-
-        elif ptr2:
-            cur.next = ptr2
-
-        return dummy.next
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
