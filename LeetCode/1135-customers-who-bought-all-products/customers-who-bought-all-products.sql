0282b7b8bdba2a78b0759ed667ce2060805eadce
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-12 14:07:14 +0300

    Time: 662 ms (16.03%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+-- FIND A CUSTOMER WHO BAOUGHT EVERYTHING FROM PRODUCT KEY
+SELECT DISTINCT
+    customer_id
+FROM Customer AS cust
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(*) FROM Product)
