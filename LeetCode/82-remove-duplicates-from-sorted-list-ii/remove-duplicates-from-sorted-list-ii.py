4ddb87ca361d93a19b4ca7f49476ccdae44a6fec
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-20 12:21:37 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (5.84%) - LeetSync

--- LeetCode/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
+++ LeetCode/82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.py
@@ -7,7 +7,7 @@ class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(-1)
         prev = dummy
-
+        # if we had empty or single element
         if not head or not head.next:
             return head
 
@@ -17,14 +17,15 @@ class Solution:
         while temp1:
             temp2 = temp1
             i, j = 0, 0
-
+            # check if the temp2 has equal value with temp1 until it finds different number
             while temp2 and temp1.val == temp2.val:
                 temp2 = temp2.next
                 j += 1
-
+            # if the movement is movment of the pointer is morethan once change the pointer
             if j - i != 1:
                 prev.next = temp2
                 temp1 = temp2
+            # if the movment is only once just don't change the pointer
             else:
                 prev.next = temp1
                 prev = temp1
