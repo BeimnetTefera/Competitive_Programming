9bc40306869d8836dc4562256042398f06c195fe
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-22 12:44:33 +0300

    Time: 256 ms (8.92%) | Memory: 40.7 MB (59.79%) - LeetSync

--- LeetCode/148-sort-list/sort-list.py
+++ LeetCode/148-sort-list/sort-list.py
@@ -6,25 +6,62 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        # if list had single element or doesn't have an elemnt return as it is cause no need to sort it
         if not head or not head.next:
             return head
 
-        nums = []
-        temp = head
+        # find the middle of the linkedlist
+        def findMiddle(head):
+            slow = head
+            fast = head.next
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
 
-        while temp:
-            nums.append(temp.val)
-            temp = temp.next
+        # merge function to connect the lists
+        def merge(head1, head2):
 
-        nums.sort()
+            dummy = ListNode(-1)
+            cur = dummy
+            
+            while head1 or head2:
 
-        new_head = ListNode(nums[0])
-        cur = new_head
+                if head1 and head2:
+                    if head1.val <= head2.val:
+                        cur.next = head1
+                        head1 = head1.next
+                    else:
+                        cur.next = head2
+                        head2 = head2.next
 
-        for i in range(1, len(nums)):
-            new_node = ListNode(nums[i])
-            cur.next = new_node
+                elif head1:
+                    cur.next = head1
+                    head1 = head1.next
 
-            cur = cur.next
+                else:
+                    cur.next = head2
+                    head2 = head2.next
 
-        return new_head
+                cur = cur.next
+
+            return dummy.next
+
+
+        # find the middle of the node
+        middle = findMiddle(head)
+
+        # left part takes the previous head
+        left_head = head
+        # right part would take the node after the middle as head
+        right_head = middle.next
+        # disconnect the left part from the right
+        middle.next = None
+
+        # do the left part until one node lefts
+        left_head = self.sortList(left_head)
+        # do the right part again until one node lefts
+        right_head = self.sortList(right_head)
+
+        # mererge the right and left part
+        return merge(left_head, right_head)
