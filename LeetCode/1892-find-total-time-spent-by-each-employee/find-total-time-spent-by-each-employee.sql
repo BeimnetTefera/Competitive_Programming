4b5c3d54840bb1af38df2394ecb9de53168d15e5
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-26 12:20:47 +0300

    Time: 784 ms (5.34%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
+++ LeetCode/1892-find-total-time-spent-by-each-employee/find-total-time-spent-by-each-employee.sql
@@ -2,17 +2,6 @@
 SELECT 
     event_day AS day,
     emp_id,
-    SUM(time_spent) AS total_time
-FROM (
-    SELECT 
-        emp_id,
-        event_day,
-        in_time,
-        out_time - in_time AS time_spent
-    FROM Employees
-    GROUP BY  emp_id,
-            event_day,
-            in_time,
-            out_time
-) AS temp
+    SUM(out_time - in_time) AS total_time
+FROM Employees
 GROUP BY emp_id, event_day
