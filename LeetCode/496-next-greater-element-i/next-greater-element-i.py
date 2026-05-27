1587431d1c4767f3e3a43292df89fcf2c4fad441
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-27 16:38:50 +0300

    Time: 19 ms (19.44%) | Memory: 19.4 MB (28.48%) - LeetSync

--- LeetCode/496-next-greater-element-i/next-greater-element-i.py
+++ LeetCode/496-next-greater-element-i/next-greater-element-i.py
@@ -1,23 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-            # populate the store with -1 vlue
-            store = {num : -1 for num in nums1}
-
-            stack = []
-            # loop through the ararray nums2 and fill tht stack in decreasing manner
-            for i in range(len(nums2)):
-                # if there is vlue in stack and the current value greater than top of stack
-                while stack and stack[-1] < nums2[i]:
-                    # remove the top of the stack
-                    stack_top = stack.pop()
-                    # if the popped value in store
-                    if stack_top in store:
-                        # store of the popped value is updated with the greater number
-                        store[stack_top] = nums2[i]
-
-                # add to the stack
-                stack.append(nums2[i])
-            # move through store values and add it to some variable
-            result = [store[num] for num in nums1]
-
-            return result
+        ans = []
+        for num in nums1:
+            idx = nums2.index(num)
+            val = None
+            for i in range(idx, len(nums2)):
+                if nums2[i] > num:
+                    val = nums2[i]
+                    ans.append(val)
+                    break
+            if not val:
+                ans.append(-1)
+        return ans
