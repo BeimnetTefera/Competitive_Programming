036569c6868bb1a37f0db4129befc0f18c39441f
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-29 15:57:07 +0300

    Time: 1267 ms (5.78%) | Memory: 20.9 MB (89.60%) - LeetSync

--- LeetCode/503-next-greater-element-ii/next-greater-element-ii.py
+++ LeetCode/503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
         
-        ans = [None] * len(nums)
+        ans = [-1] * len(nums)
 
         for i in range(len(nums)):
-
+            found = False
             for j in range(i+1, len(nums)):
                 if nums[j] > nums[i]:
                     ans[i] = nums[j]
+                    found = True
                     break
 
-            if ans[i] == None:
+            if not found:
                 for k in range (0, i):
                     if nums[k] > nums[i]:
                         ans[i] = nums[k]
                         break
-
-        for i in range(len(nums)):
-            if ans[i] == None:
-                ans[i] = -1
 
         return ans
