65c1d6f51eb385fb21c599bbc5fb0b64c6efac1b
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-31 11:33:46 +0300

    Time: 90 ms (72.13%) | Memory: 28.9 MB (34.54%) - LeetSync

--- LeetCode/739-daily-temperatures/daily-temperatures.py
+++ LeetCode/739-daily-temperatures/daily-temperatures.py
@@ -3,11 +3,11 @@ class Solution:
         ans = [0] * len(temperatures)
         stack = []
 
-        for i in range(len(temperatures)):
-            while stack and temperatures[stack[-1]] < temperatures[i]:
-                idx = stack.pop()
-                ans[idx] = i - idx
+        for cur_day in range(len(temperatures)):
+            while stack and temperatures[stack[-1]] < temperatures[cur_day]:
+                prev_day = stack.pop()
+                ans[prev_day] = cur_day - prev_day
 
-            stack.append(i)
+            stack.append(cur_day)
 
         return ans
