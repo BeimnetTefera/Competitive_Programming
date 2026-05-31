7b3fc2d79cf756ca6d304075efdda40b1283fc32
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-05-31 13:59:37 +0300

    Time: 1269 ms (5.91%) | Memory: 20.9 MB (89.71%) - LeetSync

--- LeetCode/503-next-greater-element-ii/next-greater-element-ii.py
+++ LeetCode/503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,17 +1,28 @@
 class Solution:
     def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        ans = [-1] * len(nums)
-        stack = []
+        res = []
+        n = len(nums)
 
-        for i in range(len(nums)):
-            while stack and nums[stack[-1]] < nums[i]:
-                idx = stack.pop()
-                ans[idx] = nums[i]
+        for i in range (n):
+            val = float("inf")
+            for j in range(i + 1, n):
+                if nums[j] > nums[i]:
+                    val = nums[j]
+                    break
+            if val != float("inf"):
+                res.append(val)
+            else:
+                res.append(None)
 
-            stack.append(i)
+        print(res)
 
-        for i in range(len(nums)):
-            while stack and nums[stack[-1]] < nums[i]:
-                ans[stack.pop()] = nums[i]
-
-        return ans
+        for cur_idx in range(n):
+            if res[cur_idx] == None:
+                for prev_idx in range(cur_idx):
+                    if nums[prev_idx] > nums[cur_idx]:
+                        res[cur_idx] = nums[prev_idx]
+                        break
+                if res[cur_idx] == None:
+                    res[cur_idx] = -1
+                
+        return res
