80077c35f3f3a4df62b953b7a94f7730504704d6
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-16 13:05:40 +0300

    Time: 522 ms (28.69%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/608-tree-node/tree-node.sql
+++ LeetCode/608-tree-node/tree-node.sql
@@ -3,7 +3,7 @@ SELECT
     id,
     CASE 
         WHEN p_id IS NULL THEN 'Root'
-        WHEN id NOT IN (SELECT p_id FROM Tree WHERE p_id IS NOT NULL) THEN 'Leaf'
+        WHEN NOT EXISTS (SELECT p_id FROM Tree parent WHERE node.id = parent.p_id) THEN 'Leaf'
         ELSE 'Inner'
     END AS type
-FROM Tree
+FROM Tree node
