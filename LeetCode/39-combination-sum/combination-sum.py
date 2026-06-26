917fcbe38b0ee60845ea266b59cebac4d2af1a97
Beimnet Tefera <asefabeimnet1@gmail.com>
2026-06-26 15:18:23 +0300

    Time: 7 ms (75.96%) | Memory: 19.5 MB (63.65%) - LeetSync

--- LeetCode/39-combination-sum/combination-sum.py
+++ LeetCode/39-combination-sum/combination-sum.py
@@ -2,29 +2,25 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
         store = []
+        n = len(candidates)
 
-        def helper(idx, k):
-
-            # base case
-            if idx == len(candidates) or k == 0:
-                if k == 0:
-                    ans.append(store.copy())
+        def findCombination (idx, target, store):
+            # condition
+            if idx == n:
+                if target == 0:
+                    ans.append(store[:])
 
                 return
 
-
             # pick
-            if k - candidates[idx] >= 0:
-
+            if target - candidates[idx] >= 0:
                 store.append(candidates[idx])
-                helper(idx, k - candidates[idx])
-
-                # backtrack
+                findCombination (idx, target - candidates[idx], store)
+                # not pick
                 store.pop()
 
-            # not pick
-            helper(idx + 1, k)
+            findCombination(idx + 1, target, store)
 
-        helper(0, target)
-
+        findCombination(0, target, [])
+        
         return ans
